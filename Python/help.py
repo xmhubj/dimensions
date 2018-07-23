02560bbe6320d76573d4dcea27957b44200d2c51
Xiaoming <xmhubj@gmail.com>
2018-07-23 14:21:24 +0800

    Update help.py

--- Python/help.py
+++ Python/help.py
@@ -6,3 +6,8 @@ def main():
     if len(sys.argv) == 1 or sys.argv[1] in {"-h", "--help"}:
         print("Usage: {0} file1 [file2 [...filen]]".format(sys.argv[0]))
         sys.exit()
+
+        
+print("{0} file{1}".format((count if count != 0 else "No"),
+                            ("s" if count != 1 else "")))
+
